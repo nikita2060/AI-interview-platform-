94237410730ebd44f130f094f58f123f42414355
Nikita <nikitapandey.github@gmail.com>
2025-04-28 12:04:23 +0530

    Refactor AuthForm component to enable user authentication, improve error handling, and update UI elements

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -4,20 +4,20 @@ import { z } from "zod";
 import Link from "next/link";
 import Image from "next/image";
 import { toast } from "sonner";
-// import { auth } from "@/firebase/client";
+import { auth } from "@/firebase/client";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-// import {
-//   createUserWithEmailAndPassword,
-//   signInWithEmailAndPassword,
-// } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 import { Form } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 
-// import { signIn, signUp } from "@/lib/actions/auth.action";
+import { signIn, signUp } from "@/lib/actions/auth.action";
 import FormField from "./FormField";
 
 const authFormSchema = (type: FormType) => {
@@ -41,27 +41,59 @@ const AuthForm = ({ type }: { type: FormType }) => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    try{
-      // const {name,email,password} = values
-      if(isSignIn){
-        //sign in
-        toast.success("Sign in successfully!")
-        router.push("/")
-      }else{
-        //sign up
-        toast.success("Account created successfully!Please sign in to continue")
-        router.push("/sign-in")
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    try {
+      if (type === "sign-up") {
+        const { name, email, password } = data;
+
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const result = await signUp({
+          uid: userCredential.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result.success) {
+          toast.error(result.message);
+          return;
+        }
+
+        toast.success("Account created successfully. Please sign in.");
+        router.push("/sign-in");
+      } else {
+        const { email, password } = data;
+
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const idToken = await userCredential.user.getIdToken();
+        if (!idToken) {
+          toast.error("Sign in Failed. Please try again.");
+          return;
+        }
+
+        await signIn({
+          email,
+          idToken,
+        });
+
+        toast.success("Signed in successfully.");
+        router.push("/");
       }
-      console.log(values)
-
+    } catch (error) {
+      console.log(error);
+      toast.error(`There was an error: ${error}`);
     }
-    catch (error) {
-      console.log(error)
-      toast.error(`Something went wrong :${error}`)
-  }
-}
-
+  };
 
   const isSignIn = type === "sign-in";
 
@@ -69,11 +101,11 @@ const AuthForm = ({ type }: { type: FormType }) => {
     <div className="card-border lg:min-w-[566px]">
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
-          <Image src="/logo.png" alt="logo" height={32} width={38} />
+          <Image src="/logo.svg" alt="logo" height={32} width={38} />
           <h2 className="text-primary-100">PrepWise</h2>
         </div>
 
-        <h3>Your Favourite Interviewer</h3>
+        <h3>Practice job interviews with AI</h3>
 
         <Form {...form}>
           <form
