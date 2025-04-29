ccbc89f69e355cdc0c61888b8cde3e19d10e17fa
Nikita <nikitapandey.github@gmail.com>
2025-04-29 18:08:11 +0530

    Refactor layout component, implement agent functionality, and add feedback handling logic

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,21 +1,26 @@
-import Link  from 'next/link';
-import {ReactNode }from 'react';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
+import { ReactNode } from "react";
+// import { redirect } from "next/navigation";
 
-const RootLayout =({children} :{children :ReactNode})=>{
-    return (
-        <div className='root-layout'>
-            <nav>
-                <Link href="/" className='flex items-center gap-2'>
-                    <Image src="/logo.png" alt="logo" height={32} width={38} />
-                    <h2 className='text-primary-100'>InterviewPrep</h2>
-                
-                </Link>
-                {children}
-            </nav>
-        </div>
-    )
-}
+// import { isAuthenticated } from "@/lib/actions/auth.action";
 
-export default RootLayout;
-// This is a simple React component that serves as a layout for a web application.
+const Layout = async ({ children }: { children: ReactNode }) => {
+//   const isUserAuthenticated = await isAuthenticated();
+//   if (!isUserAuthenticated) redirect("/sign-in");
+
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.png" alt="MockMate Logo" width={38} height={32} />
+          <h2 className="text-primary-100">InterviewPrep</h2>
+        </Link>
+      </nav>
+
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
