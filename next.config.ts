f234a1dd253af86d3bf7832f61fd210b3b44884d
Nikita <nikitapandey.github@gmail.com>
2025-05-14 18:36:06 +0530

    build: update dependencies and add styled-components support

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   eslint:{
-    ignoreDuringBuilds:true,
+    ignoreDuringBuilds: true,
   },
-  typescript:{
-    ignoreBuildErrors:true,
-  }
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  compiler: {
+    styledComponents: true,  // Ensures styled-components work with SSR
+  },
+  
 };
 
 export default nextConfig;
