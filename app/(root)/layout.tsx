d20064119e001ec2d8a6908f398e6e0cf156ac69
Nikita <nikitapandey.github@gmail.com>
2025-05-20 21:21:52 +0530

    refactor(layout): update logo and app name for rebranding

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -11,8 +11,8 @@ const Rootlayout = async ({children} : {children: ReactNode}) => {
     <div className='root-layout'>
       <nav className='flex items-center justify-between   shadow'>
         <Link href="/dashboard" className='flex items-center gap-2' >
-        <Image src="/logo.svg" alt="Logo" width={38} height={32}/>
-        <h2 className='text-primary-100'>IntervueAI</h2>
+        <Image src="/logo.png" alt="Logo" width={38} height={32}/>
+        <h2 className='text-primary-100'>PrepWise</h2>
         </Link>
         
        <Link href="/sign-in">
